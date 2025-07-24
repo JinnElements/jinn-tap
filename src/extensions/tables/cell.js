ba3ba76513716f063d9daff4ba3762444e929778
Martin Middel <martin@elliat.nl>
2025-07-24 09:40:47 +0200

    feat(tables): enable cell selection and simplify colspans

--- src/extensions/tables/cell.js
+++ src/extensions/tables/cell.js
@@ -41,61 +41,37 @@ export const JinnCell = Node.create({
          */
         const attributes = {};
         // Translate the TEI attribute `rows` to its HTML counterpart: `rowspan`
-        attributes.rows = {
-            default: null,
-            parseHTML: (element) => {
-                if (!element.hasAttribute('rows')) {
-                    return null;
-                }
-                return parseInt(element.getAttribute('rows'), 10);
-            },
-            renderHTML: (attributes) => (attributes.rows === 1 ? {} : { rowspan: attributes.rows }),
-        };
-
-        // Same for `cols` vs `colspan`
-        attributes.cols = {
-            default: null,
-            parseHTML: (element) => {
-                if (!element.hasAttribute('cols')) {
-                    return null;
-                }
-                return parseInt(element.getAttribute('cols'), 10);
-            },
-            renderHTML: (attributes) => (attributes.cols === 1 ? {} : { colspan: attributes.cols }),
-        };
-
-        // But they are also needed for TipTap to determine table sizes. Just don't write those back.
         attributes.rowspan = {
             default: null,
-            // When we parse XML to HTML, set it to the rows attribute
             parseHTML: (element) => {
                 if (!element.hasAttribute('rows')) {
-                    return null;
+                    return 1;
                 }
                 return parseInt(element.getAttribute('rows'), 10);
             },
             renderHTML: (attributes) => {
-                attributes.rowspan = attributes.rows ?? 1;
-                return attributes.rowspan;
+                console.log('Applying rows and stuf', attributes.rowspan);
+                attributes.rows = attributes.rowspan;
+                return {
+                    rowspan: attributes.rowspan,
+                };
             },
         };
 
         // Same for `cols` vs `colspan`
         attributes.colspan = {
             default: null,
-            // When we parse HTML back to XML, empty it
             parseHTML: (element) => {
                 if (!element.hasAttribute('cols')) {
                     return 1;
                 }
                 return parseInt(element.getAttribute('cols'), 10);
             },
-            // A bit of a hack: tiptap assumes an internal attribute 'colspan'
-            // set to whatever the colspan is. But we use cols internally. Write
-            // it to the attributes
             renderHTML: (attributes) => {
-                attributes.colspan = attributes.cols ?? 1;
-                return attributes.colspan;
+                attributes.cols = attributes.colspan;
+                return {
+                    colspan: attributes.colspan,
+                };
             },
         };
 
